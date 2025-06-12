c897984d788c6b25f11042643a1f70e1b25022f0
Artem Elizarov <somanytearsss@yandex.ru>
2025-06-13 01:11:26 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -56,3 +56,17 @@ class TestBooksCollector:
 
         assert collector.get_books_with_specific_genre("Детективы") == ["Шерлок Холмс"]
         assert collector.get_books_with_specific_genre('Фантастика') == ["Гарри Поттер"]
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book("Шерлок Холмс")
+        collector.set_book_genre("Шерлок Холмс", "Детективы")
+
+        collector.add_new_book("Гарри Поттер")
+        collector.set_book_genre("Гарри Поттер", "Фантастика")
+
+        assert collector.get_books_genre() == {
+            "Шерлок Холмс": "Детективы",
+            "Гарри Поттер": "Фантастика"
+        }
